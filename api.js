d5101bf8ab51e99a9bf6d4def754cf9c65986aa9
fah13 <fah13@hi.is>
2019-02-26 13:36:58 +0000

    Hægt að leita eftir id

--- api.js
+++ api.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const {
+  list,
+  getId,
+  update,
+} = require('./todos');
 
 /* todo importa frá todos.js */
 
@@ -8,6 +13,44 @@ function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
+async function listRoute(req, res) {
+  // const {orderby, completed } = req.query;
+
+  const items = await list();
+
+  return res.json(items);
+}
+
+async function getById(req, res) {
+  const { id } = req.params;
+  const result = await getId(id);
+
+  return res.json(result);
+}
+
+async function patchRoute(req, res) {
+  const { id } = req.params;
+  const { title, text } = req.body;
+
+  const result = await update(id, { title, text });
+
+  if (!result.success && result.notFound) {
+    return res.status(404).json({ error: 'Item not found' });
+  }
+
+  if (!result.success && result.validation.length > 0) {
+    return res.status(400).json(result.validation);
+  }
+
+
+  return res.status(200).json(result.item);
+}
+
 /* todo útfæra vefþjónustuskil */
+
+router.get('/', catchErrors(listRoute));
+router.get('/:id', catchErrors(getById));
+router.patch('/:id', catchErrors(patchRoute));
+
 
 module.exports = router;
--- todos.js
+++ todos.js
@@ -1,6 +1,110 @@
+const xss = require('xss');
+const { query } = require('./db');
 
 /* todo útfæra virkni */
 
+async function list() {
+  const result = await query('SELECT * FROM items');
+
+  return result.rows;
+}
+
+async function getId(id) {
+  const result = await query('SELECT * FROM items WHERE id = $1', [id]);
+
+  return result.rows;
+}
+
+
+function isEmpty(s) {
+  return s == null && !s;
+}
+
+function validate(title, text) {
+  const errors = [];
+
+  if (!isEmpty(title)) {
+    if (typeof title !== 'string' || title.length === 0) {
+      errors.push({
+        field: 'title',
+        error: 'Title must be a non-empty string',
+      });
+    }
+  }
+
+  if (!isEmpty(text)) {
+    if (typeof text !== 'string' || text.length === 0) {
+      errors.push({
+        field: 'text',
+        error: 'Text must be a non-empty string',
+      });
+    }
+  }
+
+  return errors;
+}
+
+async function update(id, item) {
+  const result = await query('SELECT * FROM items where id = $1', [id]);
+
+  if (result.rows.length === 0) {
+    return {
+      success: false,
+      notFound: true,
+      validation: [],
+    };
+  }
+
+  const validationResult = validate(item.title, item.text);
+
+  if (validationResult.length > 0) {
+    return {
+      success: false,
+      notFound: false,
+      validation: validationResult,
+    };
+  }
+  const changedColumns = [
+    !isEmpty(item.title) ? 'title' : null,
+    !isEmpty(item.text) ? 'text' : null,
+  ].filter(Boolean);
+
+  const changedValues = [
+    !isEmpty(item.title) ? xss(item.title) : null,
+    !isEmpty(item.text) ? xss(item.text) : null,
+  ].filter(Boolean);
+
+  const updates = [id, ...changedValues];
+
+  const updatedColumnsQuery = changedColumns.map((column, i) => `${column} = $${i + 2}`);
+
+  const q = `
+    UPDATE items
+    SET ${updatedColumnsQuery.join(', ')}
+    WHERE id = $1
+    RETURNING id, title, text`;
+
+  const updateResult = await query(q, updates);
+
+  return {
+    success: true,
+    item: updateResult.rows[0],
+  };
+}
+
+/* 
+function getTodos(completed, orderby = 'asc') {
+  const orderString = orderby.toLowerCase() === 'desc' ? 'DESC' : 'ASC';
+
+  if (completed === false || completed === 'true')
+ 
+  `SELECT id, title, ...., WHERE complted = .... ORDER BY ....`
+  `SELECT id, title, ...., ORDER BY ....`
+}
+ */
+
 module.exports = {
-  /* todo exporta virkni */
+  list,
+  getId,
+  update,
 };
